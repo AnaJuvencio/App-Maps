a21e278c2196a76c9b26956043d2310300d8d5c3
Ana Beatriz Juvencio <ana.juvencio01@gmail.com>
2020-05-28 17:55:45 -0300

    Mapa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ViewChild, ElementRef} from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+declare var google : any;
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map:any;
+  posicaoAtual:any;
+
+  @ViewChild('map', {read: ElementRef, static:false}) mapRef:ElementRef;
+
+  constructor(private geolocation: Geolocation) {}
+
+  public async showMap(){
+    //const location = new google.maps.LatLng(-22.197439, -48.775970); Posição fixa
+    await this.buscarPosicao();
+    const options = {
+      center: this.posicaoAtual,
+      zoom:15,
+      disableDefaultUI: false
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+
+    const marcador = new google.maps.Marker({
+      position: this.posicaoAtual,
+      map: this.map,
+      title: "Minha Posição Atual",
+      animation: google.maps.Animation.BOUNCE
+    });
+
+  }
+
+  ionViewDidEnter(){
+    this.showMap();
+  }
+
+  public async buscarPosicao(){
+    await this.geolocation.getCurrentPosition().then((posicaoGPS) => {
+     
+      this.posicaoAtual = {
+        lat:posicaoGPS.coords.latitude,
+        lng: posicaoGPS.coords.longitude
+      }
+      // resp.coords.latitude
+      // resp.coords.longitude
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
 
 }
